cd27e61aa15e8d634b1a049ff2418c5aaa705a07
shashank s p <shashankprince523@gmail.com>
2022-03-04 20:09:59 +0530

    Simple CountDown Timer

---
+++ app/src/main/java/com/example/countdowntimer/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.countdowntimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final long startTime = 51000;
+    private final long interval = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView Time = findViewById(R.id.times);
+        TextView timeElapsed = findViewById(R.id.timeElapsed);
+        Button reset = findViewById(R.id.resetTimer);
+        final  CountDownTimer countDownTimer = new CountDownTimer(startTime,interval) {
+            @Override
+            public void onTick(long l) {
+                Time.setText("Time: "+l/1000);
+                timeElapsed.setText("Time Elapsed: "+(startTime-l)/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Time.setText("Time Up!");
+            }
+        }.start();
+
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countDownTimer.cancel();
+                countDownTimer.start();
+            }
+        });
+    }
+}
